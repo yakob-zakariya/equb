22bc54662bea5b9fd2ae5772928b1d6f8113c8b4
yakob-zakariya <114358023+yakob-zakariya@users.noreply.github.com>
2023-06-17 02:32:11 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -4,11 +4,15 @@ const select = document.querySelector("#select");
 const lname = document.querySelector("#lname");
 const fname = document.querySelector("#fname");
 const bodyTable = document.querySelector(".bodyTable");
+const spinner = document.getElementById("spinner");
+const equbBtn = document.getElementById("start-equb-btn");
+const shuffleBtn = document.getElementById("shuffle-btn");
+const winnerName = document.getElementById("winner-name");
 
-// initiallizing members object from localStorage if it exists
+// initiallizing members object from localStorage if it exists or empty object
 const members = JSON.parse(localStorage.getItem("members")) || {};
 
-// adding a member
+// function adding a member
 const addMember = (firstName, lastName, amount) => {
   const payedDate = `${new Date().getDate()}-${
     new Date().getMonth() + 1
@@ -28,6 +32,9 @@ const myFunction = () => {
   const firstName = fname.value;
   const lastName = lname.value;
   const amount = parseInt(select.value);
+  if (firstName === "" || lastName === "" || amount === "") {
+    alert("Input field Must not be Empty... ");
+  }
   addMember(firstName, lastName, amount);
 };
 // listing to click event and passing the values to myFunction function
@@ -41,7 +48,6 @@ const addElement = () => {
     let tr = document.createElement("tr");
     let el0 = document.createElement("td");
     el0.textContent = i;
-
     let el1 = document.createElement("td");
     el1.textContent = members[value].firstName;
     let el2 = document.createElement("td");
@@ -60,3 +66,43 @@ const addElement = () => {
   }
 };
 addElement();
+
+equbBtn.addEventListener("click", () => {
+  spinner.classList.remove("hidden");
+  setTimeout(() => {
+    spinner.classList.add("hidden");
+  }, 3000);
+});
+
+//function to give a number key to a members
+const totalMoney = () => {
+  let sum = 0;
+  for (let x in members) {
+    sum += members[x].amount;
+  }
+  return sum;
+};
+const numberKey = () => {
+  let i = 1;
+  for (let member in members) {
+    members[member].number = i;
+    i++;
+  }
+  return i;
+};
+
+shuffleBtn.addEventListener("click", () => {
+  let i = numberKey() - 1;
+  let sum = totalMoney();
+  console.log(i, "number of element");
+  let j = Math.ceil(Math.random() * i);
+  console.log(j, "random");
+  for (let x in members) {
+    if (members[x].number === j) {
+      winnerName.textContent = `${members[x].firstName} ${
+        members[x].lastName
+      } won the ${sum} birr on ${new Date()}`;
+      console.log(winnerName);
+    }
+  }
+});
